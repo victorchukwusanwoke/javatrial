ac373ae848a1980bdf569ce6b48de373ecae62c8
vnwoke <120103154+Vnwoke@users.noreply.github.com>
2026-01-06 18:51:59 +0200

    recieving value from console

---
+++ Market.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Market {
+    public static void main(String[] args) {
+        Scanner market = new Scanner(System.in);
+
+        System.out.println("Give me your name: ");
+        String name = market.nextLine();
+
+        System.out.println("Give me your age: ");
+        int age = market.nextInt();
+      
+    }
+    
+}
