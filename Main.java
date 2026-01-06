245519185256c2a0f08188fd8726af4e261b47ac
vnwoke <120103154+Vnwoke@users.noreply.github.com>
2026-01-06 16:34:22 +0200

    did age variable

--- Main.java
+++ Main.java
@@ -1,5 +1,17 @@
 class Main {
     public static void main(String[] args) {
-        System.out.println("HelloWorld");
+        //System.out.println("I am Experimenting lol");
+        //So i commented the print variable
+
+        //call in an variable for age
+        int age;
+
+        //give the variable age value
+        age = 22;
+
+        //first print out for question
+        System.out.println("How old are you?"); 
+       //second printout for answer
+        System.out.println("I am " + age + "years old");
     }
 }
